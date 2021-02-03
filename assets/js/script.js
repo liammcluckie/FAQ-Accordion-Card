e15477604d72772d03ad9609a38dc8b36f980eb9
liammcluckie <liammcluckie@hotmail.co.uk>
2021-02-03 15:04:32 +0000

    Function added in script.js to rotate chevron arrows 180deg when clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,25 @@ $("#question-four").click(function() {
 $("#question-five").click(function() {
     $("#question-five").toggleClass("clicked-question");
 });
+
+//Function for flipping arrow image
+
+$("#question-one").click(function() {
+    $("#arrow-one").toggleClass("arrow-flip");
+});
+
+$("#question-two").click(function() {
+    $("#arrow-two").toggleClass("arrow-flip");
+});
+
+$("#question-three").click(function() {
+    $("#arrow-three").toggleClass("arrow-flip");
+});
+
+$("#question-four").click(function() {
+    $("#arrow-four").toggleClass("arrow-flip");
+});
+
+$("#question-five").click(function() {
+    $("#arrow-five").toggleClass("arrow-flip");
+});
