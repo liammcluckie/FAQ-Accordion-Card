5b9aa6da1a17e223a3d6c92c0b11d43c3778ba6f
liammcluckie <liammcluckie@hotmail.co.uk>
2021-02-03 10:14:09 +0000

    Toggle function added to display answers for FAQ card.

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//Function for toggling answers
+
+$("#question-one").click(function() {
+    $("#answer-one").toggle(400);
+});
+
+$("#question-two").click(function() {
+    $("#answer-two").toggle(400);
+});
+
+$("#question-three").click(function() {
+    $("#answer-three").toggle(400);
+});
+
+$("#question-four").click(function() {
+    $("#answer-four").toggle(400);
+});
+
+$("#question-five").click(function() {
+    $("#answer-five").toggle(400);
+});
